aa9966849fe1243cc5f5236ad8504921a82b255c
Karin Keijzer <karin.keijzer@gmail.com>
2018-02-16 00:39:40 +0100

    Add users. Change door to json

--- routes/door.js
+++ routes/door.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.post('/open', function(req, res, next) {
-    res.json("true");
+    res.json({open:true});
 });
 
 module.exports = router;
