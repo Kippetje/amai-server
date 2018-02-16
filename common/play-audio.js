57b847b4b12d56bfdc852fd52a2cb65a5172a3ad
Jeroen Dhondt <jeroen.dhondt@capgemini.com>
2018-02-16 19:06:28 +0100

    Test audio file

--- common/play-audio.js
+++ common/play-audio.js
@@ -1,7 +1,11 @@
 'use strict';
+var cmd = require('node-cmd');
 
 module.exports = {
     play : function (name) {
         console.log("Playing sound: " + name);
+        cmd.run('echo "Hello World"');
+        cmd.run('omxplayer ~/amai-server/private/example.amr');
+
     }
 };
