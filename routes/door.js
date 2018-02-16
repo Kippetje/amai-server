638fa3a3988f9daae7283516915526ad34eddac7
Karin Keijzer <karin.keijzer@gmail.com>
2018-02-16 16:45:54 +0100

    open close status door.

--- routes/door.js
+++ routes/door.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var doorOpen = false;
+
 router.post('/open', function(req, res, next) {
+    doorOpen = true;
     res.json({open:true});
+});
+router.post('/close', function(req, res, next) {
+    doorOpen = false;
+    res.json({open:false});
+});
+
+router.get('/status', (req, res)=>{
+    res.json(doorOpen);
 });
 
 module.exports = router;
