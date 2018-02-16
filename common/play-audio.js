cf21bbc1812b04a3e0f2221bcd79a37781684054
Jeroen Dhondt <jeroen.dhondt@capgemini.com>
2018-02-16 21:33:19 +0100

    Check if messages folder exists, graceful end if not

--- common/play-audio.js
+++ common/play-audio.js
@@ -6,13 +6,18 @@ const messageFolder = "private/messages/";
 
 module.exports = {
     play : function (receiverId, beaconId) {
-        var folder = messageFolder+receiverId+"/"+beaconId;
+        var folder = messageFolder + receiverId + "/" + beaconId;
         console.log("Searching messages for: " + receiverId + " for beacon: " + beaconId);
-        fs.readdirSync(folder).forEach(file => {
-            let command = 'omxplayer ~/amai-server/'+folder+'/'+file;
-            console.log("command: " + command);
-            cmd.run(command);
-        })
+        console.log(messageFolder+receiverId);
+        if (fs.existsSync(messageFolder+receiverId)) {
+            if(fs.existsSync(folder)) {
+                fs.readdirSync(folder).forEach(file => {
+                    let command = 'omxplayer ~/amai-server/' + folder + '/' + file;
+                    console.log("command: " + command);
+                    cmd.run(command);
+                })
+            }
+        }
 
     }
 };
