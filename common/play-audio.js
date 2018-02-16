0ff86efab0ca7fd8390ddeea7453071096a28699
Jeroen Dhondt <jeroen.dhondt@capgemini.com>
2018-02-16 17:14:00 +0100

    Add messaging route

---
+++ common/play-audio.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    play : function (name) {
+        console.log("Playing sound: " + name);
+    }
+};
--- routes/messaging.js
+++ routes/messaging.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var messages = require('../private/messages.json');
-
+let audio = require('../common/play-audio');
+let logger = require('../common/logger');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.json(messages);
 });
 
+
+router.post('/', function (req, res, next) {
+    logger.info('Received new audio file ');
+    res.json({'success':'true','message':'Saved a new audio file'});
+});
+
 router.get('/read', function(req, res, next) {
     // let user = req.query.user || "all";
-
-    if(room) {
+    if(req.query.room) {
         const room = req.query.room;
         let result = [];
         messages.forEach(function (message) {
@@ -27,5 +33,12 @@ router.get('/read', function(req, res, next) {
     }
     res.json(result);
 });
+
+router.get('/play', function(req, res, next) {
+    // let messageId = req.query.id;
+    audio.play("test");
+    res.send("Playing sound");
+});
+
 
 module.exports = router;
